113fa82b6782fa43a2432fba644e1b9c7a58fff8
Linh Macbook Pro 2019 <linh88vn@gmail.com>
2025-11-24 14:56:47 +0700

    initial nextjs landing

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-10 text-center">
+      <h1 className="text-4xl font-bold">My Startup</h1>
+      <p className="mt-4 text-lg">Welcome to our landing page 🚀</p>
+    </main>
+  );
+}
